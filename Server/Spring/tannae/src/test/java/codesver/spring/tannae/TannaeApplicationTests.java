31795f7fa9845355ea8fa4f26199d8a31eb571c9
JaeWon Choi <unsyg.choi@gmail.com>
2022-07-23 19:44:26 +0900

    [Create] TANNAE SPRING JW202207231944

---
+++ Server/Spring/tannae/src/test/java/codesver/spring/tannae/TannaeApplicationTests.java
@@ -0,0 +1,13 @@
+package codesver.spring.tannae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TannaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
